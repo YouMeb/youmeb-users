82b09012263ce3eda6a16ff43a984cd2103150e4
Po-Ying Chen <poying.me@gmail.com>
2013-10-08 23:37:14 +0800

    update config

--- test.sh
+++ test.sh
@@ -6,4 +6,4 @@ npm link ../../
 node ../../node_modules/youmeb-cli/bin/youmeb.js sequelize:migrate
 cd ../../
 
-node ./node_modules/mocha/bin/mocha test/*.test.js -t 10000
+NODE_ENV=$NODE_ENV node ./node_modules/mocha/bin/mocha test/*.test.js -t 10000
